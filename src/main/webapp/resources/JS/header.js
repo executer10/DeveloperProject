9a70701f876b87257ed4b95d2838aa6c65ca61a1
wonhana91 <corepoint91@naver.com>
2024-12-06 14:50:51 +0900

    asd

--- src/main/webapp/resources/JS/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/header.html')
-        .then(response => response.text())
-        .then(data => {
-            let headerContainer = document.createElement('div');
-            headerContainer.innerHTML = data;
-            document.body.insertBefore(headerContainer.firstChild, document.body.firstChild);
-        });
-});

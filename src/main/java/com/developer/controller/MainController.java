48830721bc62fb4705f5bbd5b4eaa7e8391fd1ba
hanol3651 <yesican3651@naver.com>
2024-12-06 12:24:03 +0900

    aaaaa

--- src/main/java/com/developer/controller/MainController.java
+++ src/main/java/com/developer/controller/MainController.java
@@ -1,10 +1,11 @@
 package com.developer.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class MainController {
 	    @GetMapping("/main")
 	    public String main() {
-		return "mainPro/main";
+		return "main/main";
 	} 
 }

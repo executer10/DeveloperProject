0d1c400e131a3671fc6c49abc8e25e921fef0dc7
CodeLibrary <115143501+executer10@users.noreply.github.com>
2024-12-10 23:16:11 +0900

    mission complete

--- src/main/java/com/developer/dto/BillingDTO.java
+++ src/main/java/com/developer/dto/BillingDTO.java
@@ -1,66 +1,111 @@
 package com.developer.dto;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class BillingDTO {
-	private int contract_number;
-	private int billing_date;
-	private Date usage_data;
+	private String contract_number;
+	private LocalDateTime billing_date;
+	private int usage_data;
 	private int usage_fee;
 	private int overdue_fee;
-	
+	private int vat_fee;
+	private int total_amount;
 	private int year;
     private int month;
-	
-	public int getContract_number() {
+	public String getContract_number() {
 		return contract_number;
 	}
-	public void setContract_number(int contract_number) {
+
+
+	public void setContract_number(String contract_number) {
 		this.contract_number = contract_number;
 	}
-	public int getBilling_date() {
+
+
+	public LocalDateTime getBilling_date() {
 		return billing_date;
 	}
-	public void setBilling_date(int billing_date) {
+
+
+	public void setBilling_date(LocalDateTime billing_date) {
 		this.billing_date = billing_date;
 	}
-	public Date getUsage_data() {
+
+
+	public int getUsage_data() {
 		return usage_data;
 	}
-	public void setUsage_data(Date usage_data) {
+
+
+	public void setUsage_data(int usage_data) {
 		this.usage_data = usage_data;
 	}
+
+
 	public int getUsage_fee() {
 		return usage_fee;
 	}
+
+
 	public void setUsage_fee(int usage_fee) {
 		this.usage_fee = usage_fee;
 	}
+
+
 	public int getOverdue_fee() {
 		return overdue_fee;
 	}
+
+
 	public void setOverdue_fee(int overdue_fee) {
 		this.overdue_fee = overdue_fee;
 	}
-	
+
+
+	public int getVat_fee() {
+		return vat_fee;
+	}
+
+
+	public void setVat_fee(int vat_fee) {
+		this.vat_fee = vat_fee;
+	}
+
+
+	public int getTotal_amount() {
+		return total_amount;
+	}
+
+
+	public void setTotal_amount(int total_amount) {
+		this.total_amount = total_amount;
+	}
+
+
 	public int getYear() {
 		return year;
 	}
+
+
 	public void setYear(int year) {
 		this.year = year;
 	}
+
+
 	public int getMonth() {
 		return month;
 	}
+
+
 	public void setMonth(int month) {
 		this.month = month;
 	}
-	
+
+
 	@Override
 	public String toString() {
 		return "BillingDTO [contract_number=" + contract_number + ", billing_date=" + billing_date + ", usage_data="
-				+ usage_data + ", usage_fee=" + usage_fee + ", overdue_fee=" + overdue_fee + ", year=" + year
-				+ ", month=" + month + "]";
+				+ usage_data + ", usage_fee=" + usage_fee + ", overdue_fee=" + overdue_fee + ", vat_fee=" + vat_fee
+				+ ", total_amount=" + total_amount + ", year=" + year + ", month=" + month + "]";
 	}
-	
 }
